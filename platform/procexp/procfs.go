fe70cb4a0d084ebf48e01cde312e14c91ba2038e
Houzuo Guo <guohouzuo@gmail.com>
2020-12-11 09:27:21 +0000

    mm

--- platform/procexp/procfs.go
+++ platform/procexp/procfs.go
@@ -3,12 +3,15 @@ package procexp
 import (
 	"encoding/binary"
 	"io/ioutil"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 const (
+	// SizeOfUint is the size of an unsigned integer - either 32 or 64.
 	SizeOfUint uint = 32 << (^uint(0) >> 63)
 )
 
@@ -18,6 +21,11 @@ var (
 	// PID, executable base name (up to 16 characters long), state, and 35 more fields.
 	// See https://man7.org/linux/man-pages/man5/procfs.5.html for the complete list of fields.
 	regexStatFields = regexp.MustCompile(`^(\d+)\s+\((.*)\)\s+(\S+)\s+` + strings.Repeat(`(\S+)\s+`, 35) + `.*$`)
+
+	// sysconfClockTick is the cached value of the number of times kernel timer interrupts each second,
+	// the value is going to be calculated by function getClockTicksPerSecondOnce.
+	sysconfClockTick           int = 0
+	getClockTicksPerSecondOnce     = new(sync.Once)
 )
 
 type ProcessStatus struct {
@@ -29,7 +37,7 @@ type ProcessStatus struct {
 	ProcessID             int
 	ParentPID             int
 	ProcessGroupID        int
-	StartedAtUptimeTick   int
+	StartedAtUptimeSec    int
 	ProcessPrivilege      ProcessPrivilege
 	ProcessMemUsage       ProcessMemUsage
 	ProcessCPUUsage       ProcessCPUUsage
@@ -47,13 +55,13 @@ type ProcessPrivilege struct {
 type ProcessMemUsage struct {
 	// stat
 	VirtualMemSizeBytes     int
-	ResidentSetMemSizePages int
+	ResidentSetMemSizeBytes int
 }
 
 type ProcessCPUUsage struct {
 	// stat
-	NumUserModeTicksInclChildren int
-	NumSysModeTicksInclChildren  int
+	NumUserModeSecInclChildren int
+	NumSysModeSecInclChildren  int
 }
 
 type ProcessSchedulerStats struct {
@@ -61,8 +69,8 @@ type ProcessSchedulerStats struct {
 	NumVoluntaryCtxSwitches    int
 	NumNonVoluntaryCtxSwitches int
 	// schedstat
-	NumRunTicks  int
-	NumWaitTicks int
+	NumRunSec  int
+	NumWaitSec int
 }
 
 // atoiOr0 returns the integer converted from the input string, or 0 if the input string does not represent a valid integer.
@@ -91,29 +99,34 @@ func getDACIDsFromProcfs(in string) []int {
 
 // getClockTicksPerSecond returns the number of times kernel timer interrupts every second.
 func getClockTicksPerSecond() int {
-	// The function body is heavily inspired by github.com/tklauser/go-sysconf
-	auxv, err := ioutil.ReadFile("/proc/self/auxv")
-	if err == nil {
-		bufPos := int(SizeOfUint / 8)
-		for i := 0; i < len(auxv)-bufPos*2; i += bufPos * 2 {
-			var tag, value uint
-			switch SizeOfUint {
-			case 32:
-				tag = uint(binary.LittleEndian.Uint32(auxv[i:]))
-				value = uint(binary.LittleEndian.Uint32(auxv[i+bufPos:]))
-			case 64:
-				tag = uint(binary.LittleEndian.Uint64(auxv[i:]))
-				value = uint(binary.LittleEndian.Uint64(auxv[i+bufPos:]))
-			}
-			switch tag {
-			// See asm/auxvec.h for the definition of constant AT_CLKTCK ("frequency at which times() increments"), which is an integer 17.
-			case 17:
-				return int(value)
+	getClockTicksPerSecondOnce.Do(func() {
+		// The function body is heavily inspired by github.com/tklauser/go-sysconf
+		auxv, err := ioutil.ReadFile("/proc/self/auxv")
+		if err == nil {
+			bufPos := int(SizeOfUint / 8)
+			for i := 0; i < len(auxv)-bufPos*2; i += bufPos * 2 {
+				var tag, value uint
+				switch SizeOfUint {
+				case 32:
+					tag = uint(binary.LittleEndian.Uint32(auxv[i:]))
+					value = uint(binary.LittleEndian.Uint32(auxv[i+bufPos:]))
+				case 64:
+					tag = uint(binary.LittleEndian.Uint64(auxv[i:]))
+					value = uint(binary.LittleEndian.Uint64(auxv[i+bufPos:]))
+				}
+				switch tag {
+				// See asm/auxvec.h for the definition of constant AT_CLKTCK ("frequency at which times() increments"), which is an integer 17.
+				case 17:
+
+					break
+					return int(value)
+				}
 			}
 		}
-	}
-	// Apparently 100 HZ is a very common value of _SC_CLK_TCK, it seems to be this way with Linux kernel 5.4.0 on x86-64.
-	return 100
+		// Apparently 100 HZ is a very common value of _SC_CLK_TCK, it seems to be this way with Linux kernel 5.4.0 on x86-64.
+		return 100
+
+	})
 }
 
 func getProcStatus(statusContent, schedstatContent, statContent string) ProcessStatus {
@@ -134,13 +147,13 @@ func getProcStatus(statusContent, schedstatContent, statContent string) ProcessS
 	uids := getDACIDsFromProcfs(statusKeyValue["Uid"])
 	gids := getDACIDsFromProcfs(statusKeyValue["Gid"])
 	return ProcessStatus{
-		Name:                statusKeyValue["Name"],
-		Umask:               statusKeyValue["Umask"],
-		ThreadGroupID:       atoiOr0(statusKeyValue["Tgid"]),
-		ProcessID:           atoiOr0(statusKeyValue["Pid"]),
-		ParentPID:           atoiOr0(statusKeyValue["PPid"]),
-		ProcessGroupID:      atoiOr0(strSliceElemOrEmpty(statFields, 5)),
-		StartedAtUptimeTick: atoiOr0(strSliceElemOrEmpty(statFields, 22)),
+		Name:               statusKeyValue["Name"],
+		Umask:              statusKeyValue["Umask"],
+		ThreadGroupID:      atoiOr0(statusKeyValue["Tgid"]),
+		ProcessID:          atoiOr0(statusKeyValue["Pid"]),
+		ParentPID:          atoiOr0(statusKeyValue["PPid"]),
+		ProcessGroupID:     atoiOr0(strSliceElemOrEmpty(statFields, 5)),
+		StartedAtUptimeSec: atoiOr0(strSliceElemOrEmpty(statFields, 22)) / getClockTicksPerSecond(),
 		ProcessPrivilege: ProcessPrivilege{
 			RealUID:      uids[0],
 			EffectiveUID: uids[1],
@@ -149,17 +162,17 @@ func getProcStatus(statusContent, schedstatContent, statContent string) ProcessS
 		},
 		ProcessMemUsage: ProcessMemUsage{
 			VirtualMemSizeBytes:     atoiOr0(strSliceElemOrEmpty(statFields, 23)),
-			ResidentSetMemSizePages: atoiOr0(strSliceElemOrEmpty(statFields, 24)),
+			ResidentSetMemSizeBytes: atoiOr0(strSliceElemOrEmpty(statFields, 24)) * os.Getpagesize(),
 		},
 		ProcessCPUUsage: ProcessCPUUsage{
-			NumUserModeTicksInclChildren: atoiOr0(strSliceElemOrEmpty(statFields, 14)) + atoiOr0(strSliceElemOrEmpty(statFields, 16)),
-			NumSysModeTicksInclChildren:  atoiOr0(strSliceElemOrEmpty(statFields, 15)) + atoiOr0(strSliceElemOrEmpty(statFields, 17)),
+			NumUserModeSecInclChildren: atoiOr0(strSliceElemOrEmpty(statFields, 14)) + atoiOr0(strSliceElemOrEmpty(statFields, 16))/getClockTicksPerSecond(),
+			NumSysModeSecInclChildren:  atoiOr0(strSliceElemOrEmpty(statFields, 15)) + atoiOr0(strSliceElemOrEmpty(statFields, 17))/getClockTicksPerSecond(),
 		},
 		ProcessSchedulerStats: ProcessSchedulerStats{
 			NumVoluntaryCtxSwitches:    atoiOr0(statusKeyValue["voluntary_ctxt_switches"]),
 			NumNonVoluntaryCtxSwitches: atoiOr0(statusKeyValue["nonvoluntary_ctxt_switches"]),
-			NumRunTicks:                atoiOr0(strSliceElemOrEmpty(schedstatFields, 1)),
-			NumWaitTicks:               atoiOr0(strSliceElemOrEmpty(schedstatFields, 2)),
+			NumRunSec:                  atoiOr0(strSliceElemOrEmpty(schedstatFields, 1)) / getClockTicksPerSecond(),
+			NumWaitSec:                 atoiOr0(strSliceElemOrEmpty(schedstatFields, 2)) / getClockTicksPerSecond(),
 		},
 	}
 }
